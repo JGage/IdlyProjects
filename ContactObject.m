d1a62d904339235955945861af7568bf17ca1791
JGage <JGage@users.noreply.github.com>
2014-11-04 09:56:27 -0500

    Create ContactObject.m

---
+++ ContactObject.m
@@ -0,0 +1,13 @@
+//
+//  ContactObject.m
+//  ContactAccessProject2
+//
+//  Created by josh on 9/29/14.
+//  Copyright (c) 2014 GageIT. All rights reserved.
+//
+
+#import "ContactObject.h"
+
+@implementation ContactObject
+
+@end
