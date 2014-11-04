f20e0df9649542fee930e1d6552d6a0c5d9b4f33
JGage <JGage@users.noreply.github.com>
2014-11-04 09:51:58 -0500

    Create MySignupViewController.h

---
+++ MySignupViewController.h
@@ -0,0 +1,14 @@
+//
+//  MySignupViewController.h
+//  Altogether
+//
+//  Created by josh on 10/26/14.
+//  Copyright (c) 2014 GageIT. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+#import "MyLoginViewController.h"
+
+@interface MySignupViewController : PFSignUpViewController
+
+@end
