91e6c7732552e92b9ba3f402876e4eacdfc4a7cf
JGage <JGage@users.noreply.github.com>
2014-11-04 09:50:23 -0500

    Create UIColor+Colors.h

---
+++ UIColor+Colors.h
@@ -0,0 +1,24 @@
+//
+//  UIColor+Colors.h
+//  Altogether
+//
+//  Created by josh on 10/29/14.
+//  Copyright (c) 2014 GageIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Colors)
+
+
+// Basement Grey - DEMO
++ (UIColor *) DEMObasementGray;
+// Basement Grey - Custom
++ (UIColor *) CONbasementGrey;
+// Idly Blue
++ (UIColor *) CONidlyPaleBlue;
+// Idly Pink
++ (UIColor *) CONidlyPalePink;
+
+
+@end
