eb150897e81c906ca6713ce635fbae33ee999a9e
JGage <JGage@users.noreply.github.com>
2014-11-04 09:54:14 -0500

    Create SidebarViewController.h

---
+++ SidebarViewController.h
@@ -0,0 +1,15 @@
+
+#import <UIKit/UIKit.h>
+// This provides us w/ the necessary libraries & classes to work with address book contacts
+#import <AddressBook/AddressBook.h>
+// This is required to display address book navigation controller & all existing contacts
+#import <AddressBookUI/AddressBookUI.h>
+#import "MyTableViewController.h"
+//#import "SegueContactsList.h"
+#import "ContactObject.h"
+#import "Vars AND Constants.h"
+
+@interface SidebarViewController : UITableViewController < UIAlertViewDelegate >
+
+
+@end
